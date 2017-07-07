a1cfd5f753baa3b0e3d0be8f40d2c7cf07b2b263
Quentin Midy <q.midy@lectra.com>
2017-07-07 13:50:58 +0200

    Ajout des contrôleurs REST User et Player

---
+++ ThatsMyBandRest/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThatsMyBandInfrastructure;
+
+namespace ThatsMyBandRest.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlayerController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Player> Get()
+        {
+            return new Player[] { new Player { Name = "Name1" } };
+        }
+    }
+}
---
+++ ThatsMyBandRest/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThatsMyBandInfrastructure;
+
+namespace ThatsMyBandRest.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new User[] { new User { Id = new Guid() } };
+        }
+    }
+}
